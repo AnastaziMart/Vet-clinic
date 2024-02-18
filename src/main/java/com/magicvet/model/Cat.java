855829471bf737cc102da4beba85bff3c4d8c2d1
AnastaziMart <anastasiamartus474@gmail.com>
2024-02-19 00:14:02 +0100

    REQ-07: Use enum for dog size

--- src/main/java/com/magicvet/model/Cat.java
+++ src/main/java/com/magicvet/model/Cat.java
@@ -3,4 +3,7 @@ package main.java.com.magicvet.model;
 public class Cat extends Pet {
 
 
+    public Cat(String cat, String female, String number, String misty, String anastasiaMartus) {
+
+    }
 }
